f2ac861f858c4ed1513e5d7c3fd792ad526f3318
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-04-11 22:09:53 -0300

    Dividindo a entrada e saida em dois arquivos

---
+++ entrada-saida/saida.py
@@ -0,0 +1,9 @@
+# A função print() é usada para exibir uma mensagem na tela.
+print("Olá, mundo!")
+print("O resultado é:", 42)
+print("2 + 2 =", 2 + 2)
+
+# Também é possível formatar a saída usando as strings formatadas.
+nome = "João"
+idade = 30
+print(f"{nome} tem {idade} anos.")
