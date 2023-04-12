7186eeef43bf95b233685046d9cb98875d32bfb5
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-04-11 22:14:17 -0300

    Adicionando mais exemplos de condicionais

---
+++ condicionais/case.py
@@ -0,0 +1,12 @@
+# Em Python, não existe uma estrutura de controle de fluxo switch case como em algumas outras linguagens de programação, como C e Java. 
+# Em vez disso, pode-se usar uma série de declarações if/elif para simular o comportamento do switch case.
+opcao = 3
+
+if opcao == 1:
+    print("Opção 1 selecionada")
+elif opcao == 2:
+    print("Opção 2 selecionada")
+elif opcao == 3:
+    print("Opção 3 selecionada")
+else:
+    print("Opção inválida")
